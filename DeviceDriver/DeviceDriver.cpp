bb15a902eede3538a4bc4353f81664d37b236b4e
hwangyu.kim <skykhk01@gmail.com>
2023-06-05 15:38:43 +0900

    Add feature/Read code

--- DeviceDriver/DeviceDriver.cpp
+++ DeviceDriver/DeviceDriver.cpp
@@ -1,15 +1,24 @@
 #include "DeviceDriver.h"
+#include <exception>
+#include <windows.h>
+
+using namespace std;
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware)
 	: m_hardware(hardware)
 {
-	
+
 }
 
 int DeviceDriver::read(long address)
 {
-	// TODO: implement this method properly
-	return (int)(m_hardware->read(address));
+	int value = (int)(m_hardware->read(address));
+	for (int readCnt = 1; readCnt < TOTAL_READ_COUNT; ++readCnt) {
+		Sleep(READ_INTERVAL);
+		if (value == (int)(m_hardware->read(address))) continue;
+		throw std::exception("ReadFailException!");
+	}
+	return value;
 }
 
 void DeviceDriver::write(long address, int data)
